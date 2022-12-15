7ff6ea79e14ea228b89b0a56ca1286c249fb2e84
Darcyu83 <darcy.u83@gmail.com>
2022-12-15 16:39:54 +0900

    VERCEL 주소로 바꿈

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,6 @@
 export const PREFIX_HOME =
   process.env.NODE_ENV === 'production'
-    ? 'https://nextjs-movies-g9aa6tdpi-darcyu83.vercel.app'
+    ? 'https://nextjs-movies-silk.vercel.app'
     : 'http://localhost:3000';
 
 export const PREFIX_POSTER = 'https://image.tmdb.org/t/p/w500';
--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
   },
   assetPrefix:
     process.env.NODE_ENV === 'production'
-      ? 'https://nextjs-movies-g9aa6tdpi-darcyu83.vercel.app'
+      ? 'https://nextjs-movies-silk.vercel.app'
       : '',
   trailingSlash: true,
 };
