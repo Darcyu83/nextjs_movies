e35b4c1283ea200f04690179a01cc9bbea52b2f6
Darcyu83 <darcy.u83@gmail.com>
2022-12-15 17:34:55 +0900

    개발/프로덕트 모드 확인 문구 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,7 @@ export default function Home() {
         <div className={styles.grid}>
           <Link className={styles.card} href={`${PREFIX_HOME}/ssr`}>
             <p>Server Side Rendered List</p>
-            <p>
-              {PREFIX_HOME} -=-=- {process.env.NODE_ENV}
-            </p>
+            <p>{`${PREFIX_HOME} ===--=== ${process.env.NODE_ENV}`}</p>
           </Link>
           <Link className={styles.card} href={`${PREFIX_HOME}/csr`}>
             <p>Client Side Rendered List</p>
