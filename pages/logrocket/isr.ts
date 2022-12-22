ef089a0e90be3e09a8201446a9dc2c24c91c1eba
Darcyu83 <darcy.u83@gmail.com>
2022-12-22 19:25:24 +0900

    revalidate: 60 적용

--- pages/logrocket/isr.ts
+++ pages/logrocket/isr.ts
@@ -15,6 +15,6 @@ export const getStaticProps: GetStaticProps = async () => {
         pageDesc: `To Check the effect, Please clear the browser cache. \n(1) 첫 요청시 데이터 업데이트 \n(2) 모든 유저에게 10초동안 업데이트된 같은 화면 제공 \n(3) 10초 경과 후 첫 요청에 새로운 데이터 업데이트 \nrevalidate: 10s in this page / SSG like with an additional revalidate property that means how often to revalidate the page(in seconds).`,
       },
     },
-    revalidate: 10,
+    revalidate: 60,
   };
 };
